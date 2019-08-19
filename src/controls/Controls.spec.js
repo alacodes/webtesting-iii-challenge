26c9d663ce40d4bc8eaaf82ce00ae017e37a7933
Tracie T <traciecodes@gmail.com>
2019-08-19 17:34:02 -0500

    Completed snapshot setup for dashboard and controls. MVP reqs met

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,5 +1,50 @@
 // Test away!
 import React from 'react'
-import { render } from '@testing-library/react'
-import '@testing-library/react/cleanup-after-each'
+import { render, fireEvent } from '@testing-library/react'
+import 'jest-dom/extend-expect'
+import renderer from 'react-test-renderer'
+import { toBeDisabled } from 'jest-dom'
 import Controls from './Controls'
+
+describe('<Control />', () => {
+    //snapshot: component
+    //this sick move generates a DOM tree
+    //Remember: snapshots are a JSON rep. of the DOM tree
+
+    it('matches the snapshot', () =>{
+        const tree = renderer.create(<Controls />)
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+
+    expect.extend({toBeDisabled})
+
+    //disable lock/unlock while gate is open
+    it('should disable lock/unlock button while the gate is open', ()=> {
+        const {getByText} = render(<Controls closed={false}/>)
+        const button = getByText(/lock gate/i)
+        expect(button).toBeDisabled()
+    })
+
+    //do the same for the open/close button when closed
+    it('should disable open/close button when locked', () => {
+        const{getByText} = render(<Controls locked={true}/>)
+        const button = getByText(/close gate/i)
+        expect(button).toBeDisabled()
+    })
+
+    //button render for closed and locked
+    it('should render a button to toggle closed and locked', () => {
+        const {getByText} = render(<Controls />)
+        getByText(/close gate/i)
+        getByText(/lock gate/i)
+    })
+
+    //button toggle to change on click
+    it('should toggle on button click', () => {
+        const toggleClosed = jest.fn()
+        const {getByText} = render(<Controls toggleClosed={toggleClosed}/>)
+        const button = getByText(/close gate/i)
+        fireEvent.click(button)
+        expect(toggleClosed).toHaveBeenCalledTimes(1)
+    })
+})
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,10 +1,20 @@
 // Test away
 import React from 'react'
 import { render } from '@testing-library/react'
-import '@testing-library/react/cleanup-after-each'
+import renderer from 'react-test-renderer'
 import Dashboard from './Dashboard'
 
 describe('<Dashboard />', () => {
+
+    //snapshot attempt, wish me luck
+    //this sick move generates a DOM tree
+    //Remember: snapshots are a JSON rep. of the DOM tree
+
+    it('matches the snapshot', () => {
+        const tree = renderer.create(<Dashboard />)
+        expect(tree.toJSON()).toMatchSnapshot()
+    })
+    
     it('should render controls with display', () => {
         const { getByText } = render(<Dashboard />)
             getByText(/unlocked/i)
