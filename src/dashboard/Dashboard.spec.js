2a19f0d7fe899049be8f9a10fdc98347828ccb44
Tracie T <traciecodes@gmail.com>
2019-08-19 15:37:43 -0500

    Upgraded yarn, attempted setup test for the dashboard re:controls

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -3,3 +3,13 @@ import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/react/cleanup-after-each'
 import Dashboard from './Dashboard'
+
+describe('Dashboard Check', () => {
+    it('should render controls with display', () => {
+        const { getByText } = render(<Dashboard />)
+            getByText(/unlocked/i)
+            getByText(/lock gate/i)
+            getByText(/open/i)
+            getByText(/close gate/i)
+    })
+})
